413a87f61ec94f2299120b181ff168988ba64e24
monirhosenakanda <monirhosenakanda@gmail.com>
2022-05-05 20:12:31 +0600

    database conneted

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+//database connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpx0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("Book Service DB Connected");
+  // perform actions on the collection object
+  client.close();
+});
+
 //middle ware
 app.use(cors());
 app.use(express.json());
